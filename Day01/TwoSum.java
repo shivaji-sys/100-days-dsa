55f6cf1bbd2a79459ce2c4e2d3451fd6b5f09c61
shiva-dev-Dsa <shivas221205@gmail.com>
2026-02-05 14:16:48 +0530

    Implement Friends Pairing Problem solution

---
+++ Day01/TwoSum.java
@@ -0,0 +1,21 @@
+Day 1: Friends Pairing Problem
+
+
+  class Solution {
+    public long countFriendsPairings(int n) {
+        // code here
+        if(n==1 || n==2){
+            return n;
+        }
+        
+        long fnm1 = countFriendsPairings(n-1);
+        long fnm2 = countFriendsPairings(n-2);
+    
+        long pairways = (n-1)*fnm2;
+        
+        long totaways = fnm1 + pairways;
+    
+        return totaways;
+    }
+    
+}
